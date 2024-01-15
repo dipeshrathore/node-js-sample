77148f4cf7ea7c8a7607f622bcce6c38b0134a13
dipeshrathore <70268563+dipeshrathore@users.noreply.github.com>
2024-01-15 18:47:18 +0530

    Update git_versioning_tag.sh

--- git_versioning_tag.sh
+++ git_versioning_tag.sh
@@ -28,17 +28,12 @@ VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-echo "*****************VNUM1: ${VNUM1}"
 if [[ $VERSION == 'major' ]]
 then
   VNUM1=v$((VNUM1+1))
-  echo "*****************after updating VNUM1: ${VNUM1}"
-  VNUM2=0
-  VNUM3=0
 elif [[ $VERSION == 'minor' ]]
 then
   VNUM2=$((VNUM2+1))
-  VNUM3=0
 elif [[ $VERSION == 'patch' ]]
 then
   VNUM3=$((VNUM3+1))
@@ -68,4 +63,3 @@ fi
 echo ::set-output name=git-tag::$NEW_TAG
 
 exit 0
-
