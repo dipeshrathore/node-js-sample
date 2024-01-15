0007c975b652deb7ba7bcd8b7009770312cf58f8
dipeshrathore <70268563+dipeshrathore@users.noreply.github.com>
2024-01-15 18:21:28 +0530

    Update git_versioning_tag.sh

--- git_versioning_tag.sh
+++ git_versioning_tag.sh
@@ -5,9 +5,9 @@ VERSION=""
 # get parameters
 while getopts v: flag
 do
-  case "${flag}" in
-    v) VERSION=${OPTARG};;
-  esac
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
 done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
@@ -16,7 +16,7 @@ CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v0.1.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -30,19 +30,19 @@ VNUM3=${CURRENT_VERSION_PARTS[2]}
 
 if [[ $VERSION == 'major' ]]
 then
-  VNUM1=$((VNUM1+1))
-  VNUM2=0
-  VNUM3=0
+  VNUM1=$((VNUM1+1))
+  VNUM2=0
+  VNUM3=0
 elif [[ $VERSION == 'minor' ]]
 then
-  VNUM2=$((VNUM2+1))
-  VNUM3=0
+  VNUM2=$((VNUM2+1))
+  VNUM3=0
 elif [[ $VERSION == 'patch' ]]
 then
-  VNUM3=$((VNUM3+1))
+  VNUM3=$((VNUM3+1))
 else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-  exit 1
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
 fi
 
 # create new tag
@@ -55,12 +55,12 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
-  echo "Tagged with $NEW_TAG"
-  git tag $NEW_TAG
-  git push --tags
-  git push
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
 else
-  echo "Already a tag on this commit"
+  echo "Already a tag on this commit"
 fi
 
 echo ::set-output name=git-tag::$NEW_TAG
